08f7bf25be4303bc97502205a09cdab99c553f3b
Zil Norvilis <zilasino27@gmail.com>
2021-05-16 21:07:16 +0200

    Add custom map

--- src/assets/map/spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="spritesheet" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../DEV/MICROVERSE/05-JAVASCRIPT/capstone/rpg-game/dist/assets/map/spritesheet.png" width="32" height="48"/>
-</tileset>
